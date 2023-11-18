90b8a102c9903a5b9879e3297b7c030a75d83936
tarik razine <tarikrazine.com@gmail.com>
2023-11-19 00:01:11 +0100

    fear: starting with sidebar

--- src/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -21,6 +21,31 @@ function Sidebar({ storageKey = "t-sidebar-storage" }: SidebarProps) {
     [],
   );
 
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: { infinite: true },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    [],
+  );
+
+  function onExpand(id: string) {
+    setExpanded((expanded) => ({
+      ...expanded,
+      [id]: !expanded[id],
+    }));
+  }
+
   return <div>sidebar</div>;
 }
 
