a229d69f4c93e3cdf654cf1eafb2671a102682f4
tarik razine <tarikrazine.com@gmail.com>
2023-11-25 00:44:58 +0100

    feat: useAction

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,3 @@
-import { createBoard } from "@/actions/createBoard";
-import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 
 export default async function OrganizationId({
@@ -11,17 +9,6 @@ export default async function OrganizationId({
 
   return (
     <div className="flex flex-col space-y-4">
-      <form action={createBoard}>
-        <input
-          id="title"
-          type="text"
-          name="title"
-          placeholder="Enter a board title"
-          className="border border-black p-1"
-          required
-        />
-        <Button type="submit">Create</Button>
-      </form>
       <div className="space-y-2">
         {boards.map((board) => (
           <div key={board.id}>Board name: {board.title}</div>
